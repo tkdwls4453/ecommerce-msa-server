559d4df247a08958430047d2460334ae10acf7fc
Yoon321 <tkdwls734@gmail.com>
2025-01-11 11:44:03 +0900

    fix: 새로운 프로젝트 셋팅 #10
    클론시 루트 프로젝트 인식 못하는 문제로 프로젝트를 새로 셋팅했습니다.
    루트 프로젝트 인식을 위해 해당 디렉토리를 남겨두고 추후에 삭제 예정

---
+++ src/main/java/com/commerce/ecommercemsaserver/EcommerceMsaServerApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.ecommercemsaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMsaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMsaServerApplication.class, args);
+	}
+
+}
