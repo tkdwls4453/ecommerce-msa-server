e66e2972b85815e834e9a9e938b9d996f7acae4e
kjs4231 <kjs4231@naver.com>
2025-01-11 21:12:02 +0900

    feat: ListRedisTemplates #22
    ListRedisTemplate에 각방향의 add,pop 인덱스 찾기와 업데이트, 리스트조회 및 삭제 추상화 작업했습니다.

--- common/src/main/java/com/msa/common/redis/JsonValueRedisTemplate.java
+++ common/src/main/java/com/msa/common/redis/JsonValueRedisTemplate.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class JsonValueRedisTemplate {
@@ -24,4 +26,51 @@ public class JsonValueRedisTemplate {
         String stringValue = redisTemplate.opsForValue().get(key);
         return Optional.ofNullable(mapper.readValue(stringValue, clazz));
     }
+
+    public <T> void addLeftToList(String key, T vlaue) throws JsonProcessingException {
+        String stringValue = mapper.writeValueAsString(vlaue);
+        redisTemplate.opsForList().leftPush(key,stringValue);
+    }
+    public <T> void addRightToList(String key, T vlaue) throws JsonProcessingException {
+        String stringValue = mapper.writeValueAsString(vlaue);
+        redisTemplate.opsForList().rightPush(key,stringValue);
+    }
+
+    public <T> Optional<T> popLeftFromList(String key, Class<T> clazz) throws JsonProcessingException {
+        String stringValue = redisTemplate.opsForList().leftPop(key);
+        return Optional.ofNullable(mapper.readValue(stringValue,clazz));
+    }
+    public <T> Optional<T> popRightFromList(String key, Class<T> clazz) throws JsonProcessingException {
+        String stringValue = redisTemplate.opsForList().rightPop(key);
+        return Optional.ofNullable(mapper.readValue(stringValue,clazz));
+    }
+
+    public <T> List<T> getAllList(String key, Class<T> clazz) throws JsonProcessingException {
+        List<String> stringValues = redisTemplate.opsForList().range(key, 0, -1);
+        List<T> resultSet = new ArrayList<>();
+
+        if (stringValues != null) {
+            for (String stringValue : stringValues) {
+                T value = mapper.readValue(stringValue, clazz);
+                resultSet.add(value);
+            }
+        }
+
+        return resultSet;
+    }
+    public <T> void removeFromList(String key, T value) throws JsonProcessingException{
+        String stringValue = mapper.writeValueAsString(value);
+        redisTemplate.opsForList().remove(key,1,stringValue);
+    }
+
+    public <T> Optional<T> getIndexAt(String key, long index, Class<T> clazz) throws JsonProcessingException {
+        String stringValue = redisTemplate.opsForList().index(key, index);
+        return Optional.ofNullable(mapper.readValue(stringValue, clazz));
+    }
+
+    public <T> void updateIndexAt(String key, long index, T value) throws JsonProcessingException {
+        String stringValue = mapper.writeValueAsString(value);
+        redisTemplate.opsForList().set(key, index, stringValue);
+    }
+
 }
