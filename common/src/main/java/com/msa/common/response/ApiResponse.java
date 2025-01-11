09d737de894a65c07e728755ebd33dfc62b09267
Yoon321 <tkdwls734@gmail.com>
2025-01-11 23:16:08 +0900

    refactor: ApiResponse 코드 수정 # 16
    StatusCode 인터페이스를 인자로 사용하여 여러 상태 코드 구현체가 응답을 생성할 수 있도록 수정하였습니다.

--- common/src/main/java/com/msa/common/response/ApiResponse.java
+++ common/src/main/java/com/msa/common/response/ApiResponse.java
@@ -3,6 +3,7 @@ package com.msa.common.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -24,37 +25,42 @@ public class ApiResponse<T> {
 
     // 성공 응답
     public static <T> ApiResponse<T> success(String message, T data) {
-        return createApiResponse(ResponseStatus.SUCCESS.toString(), "S200", message, data, null);
+        return createApiResponse(GlobalStatusCode.SUCCESS, GlobalStatusCode.SUCCESS.getCode(), message, data, null);
     }
 
     public static <T> ApiResponse<T> success(T data) {
-        return createApiResponse(ResponseStatus.SUCCESS.toString(), "S200", "OK", data, null);
+        return createApiResponse(GlobalStatusCode.SUCCESS, GlobalStatusCode.SUCCESS.getCode(), null, data, null);
     }
 
     public static ApiResponse<Void> success(String message) {
-        return createApiResponse(ResponseStatus.SUCCESS.toString(), "S200", message, null, null);
+        return createApiResponse(GlobalStatusCode.SUCCESS, GlobalStatusCode.SUCCESS.getCode(), message, null, null);
     }
 
     public static ApiResponse<Void> success() {
-        return createApiResponse(ResponseStatus.SUCCESS.toString(), "S200", "OK", null, null);
+        return createApiResponse(GlobalStatusCode.SUCCESS, GlobalStatusCode.SUCCESS.getCode(), null, null, null);
     }
 
     // 실패 응답
-    public static <T> ApiResponse<T> failure(String code, String message, T errors) {
-        return createApiResponse(ResponseStatus.FAIL.toString(), code, message, null, errors);
+    public static <T> ApiResponse<T> failure(StatusCode code, String message, T errors) {
+        return createApiResponse(GlobalStatusCode.FAIL, code.getCode(), message, null, errors);
     }
 
-    public static <T> ApiResponse<T> failure(String code, T errors) {
-        return createApiResponse(ResponseStatus.FAIL.toString(), code, null, null, errors);
+    public static <T> ApiResponse<T> failure(T errors) {
+        return createApiResponse(GlobalStatusCode.FAIL, GlobalStatusCode.FAIL.getCode(), null, null, errors);
     }
 
-    public static ApiResponse<Void> failure(String code, String message) {
-        return createApiResponse(ResponseStatus.FAIL.toString(), code, message, null, null);
+    public static ApiResponse<Void> failure(StatusCode code) {
+        return createApiResponse(GlobalStatusCode.FAIL, code.getCode(), code.getMessage(), null, null);
     }
 
-    private static <T> ApiResponse<T> createApiResponse(String status, String code, String message, T data, T errors) {
+    // 에러 응답
+    public static ApiResponse<Void> error() {
+        return createApiResponse(GlobalStatusCode.ERROR, GlobalStatusCode.ERROR.getCode(), GlobalStatusCode.ERROR.getMessage(), null, null);
+    }
+
+    private static <T> ApiResponse<T> createApiResponse(StatusCode responseStatus, String code, String message, T data, T errors) {
         return ApiResponse.<T>builder()
-            .status(status)
+            .status(responseStatus.toString())
             .code(code)
             .message(message)
             .data(data)
