afb35144c8a11e01ba44b1030a1703086d5ccdbc
Yoon321 <tkdwls734@gmail.com>
2025-01-15 14:55:16 +0900

    feat: 스프링 시큐리티에서 모든 요청 통과 #25
    추후에 API GATEWAY 에서 라우팅할 때 인증할 예정으로 여기서는 모두 통과시킴

--- user-service/src/main/java/com/msa/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/msa/user/config/SecurityConfig.java
@@ -2,12 +2,31 @@ package com.msa.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/**").permitAll()
+            )
+            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
     }
 }
