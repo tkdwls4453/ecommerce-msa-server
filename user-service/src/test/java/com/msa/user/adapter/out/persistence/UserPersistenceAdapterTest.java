1368287500b28d7dbaa8620db631f6937b063223
Yoon321 <tkdwls734@gmail.com>
2025-01-21 09:37:23 +0900

    test: 유저 어댑터의 빠진 테스트 코드 추가
    이메일 존재 여부, 이메일 조회 등 작성하지 못했던 테스트 코드를 추가했습니다.

--- user-service/src/test/java/com/msa/user/adapter/out/persistence/UserPersistenceAdapterTest.java
+++ user-service/src/test/java/com/msa/user/adapter/out/persistence/UserPersistenceAdapterTest.java
@@ -47,8 +47,8 @@ class UserPersistenceAdapterTest {
     }
 
     @Test
-    @DisplayName("유저 조회시, 엔티티를 도메인으로 변경하여 리턴한다.")
-    void shouldConvertEntityToDomainWhenFetchingUser(){
+    @DisplayName("존재하는 이메일로 유저 조회시, 엔티티를 도메인으로 변경하여 리턴한다.")
+    void shouldConvertEntityToDomainWhenFetchingUser() {
         // Given
         User user = UserFixtures.user();
         UserEntity savedUserEntity = UserFixtures.userEntity(1L, user);
@@ -68,5 +68,52 @@ class UserPersistenceAdapterTest {
         assertThat(savedUser.getPassword()).isEqualTo(user.getPassword());
 
         verify(userJpaRepository).findByEmail(user.getEmail());
+    }
+
+    @Test
+    @DisplayName("존재하지 않은 이메일로 유저 조회시, Optional.empty 리턴한다.")
+    void shouldConvertEntityToDomainWhenFetchingUser_ReturnEmpty() {
+        // Given
+        String nonExistsEmail = "nonExistsEmail";
+
+        when(userJpaRepository.findByEmail(nonExistsEmail)).thenReturn(
+            Optional.empty()
+        );
+
+        // When
+        User savedUser = sut.findByEmail(nonExistsEmail).orElse(null);
+
+        // Then
+        assertThat(savedUser).isNull();
+        verify(userJpaRepository).findByEmail(nonExistsEmail);
+    }
+
+
+    @Test
+    @DisplayName("이메일이 존재할 때 이메일 존재를 확인하면 true 를 리턴한다.")
+    void existsByEmail_returnTrue(){
+        // Given
+        String email = "test123@naver.com";
+
+        when(userJpaRepository.existsByEmail(email)).thenReturn(true);
+
+        // When
+        Boolean result = sut.existsByEmail(email);
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("이메일이 존재하지 않을 때 이메일 존재를 확인하면 false 를 리턴한다.")
+    void existsByEmail_returnFalse(){
+        // Given
+        String nonExistsEmail = "nonexist@naver.com";
+
+        when(userJpaRepository.existsByEmail(nonExistsEmail)).thenReturn(false);
+
+        // When
+        Boolean result = sut.existsByEmail(nonExistsEmail);
+        // Then
+        assertThat(result).isFalse();
     }
 }
