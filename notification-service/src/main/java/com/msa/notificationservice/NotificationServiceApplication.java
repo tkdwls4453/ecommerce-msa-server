d66bdf00ab03154c6a4e99a77315a029a72c8129
LSG <sskkilm@naver.com>
2025-01-23 23:30:29 +0900

    feat: notification service module init

---
+++ notification-service/src/main/java/com/msa/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.msa.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.msa.notificationservice", "com.msa.common"})
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
