aa9f3303cdf3bb959de79f2cab54a78a0f4498f7
Yoon321 <tkdwls734@gmail.com>
2025-02-01 21:26:49 +0900

    feat: 주문 도메인 관련 기능 개발 #40
    - 주문 초기화
    - 주문 실패
    - 주문 진행

--- order-service/src/main/java/com/msa/order/domain/Order.java
+++ order-service/src/main/java/com/msa/order/domain/Order.java
@@ -1,9 +1,13 @@
 package com.msa.order.domain;
 
+import com.msa.order.application.port.in.CreateNewOrderCommand;
 import com.msa.order.domain.vo.AppliedCoupon;
 import com.msa.order.domain.vo.Money;
 import com.msa.order.domain.vo.OrderShoes;
 import com.msa.order.domain.vo.ShippingInfo;
+import com.msa.order.exception.InvalidCouponTypeException;
+import com.msa.order.exception.InvalidTotalPriceException;
+import com.msa.order.exception.NoOrderItemException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -36,5 +40,63 @@ public class Order {
         this.orderStatus = orderStatus;
         this.totalPrice = totalPrice;
         this.orderTime = orderTime;
+    }
+
+    public static Order init(Long customerId, CreateNewOrderCommand command, LocalDateTime orderTime, UUID orderCode) {
+        verifyTotalPrice(command.orderLine(), command.coupon(), command.totalPrice());
+
+        return Order.builder()
+            .orderCode(orderCode)
+            .customerId(customerId)
+            .appliedCoupon(command.coupon())
+            .shippingInfo(command.shippingInfo())
+            .orderLine(command.orderLine())
+            .orderStatus(OrderStatus.ORDER_RECEIVED)
+            .totalPrice(command.totalPrice())
+            .orderTime(orderTime)
+            .build();
+    }
+
+    private static void verifyTotalPrice(List<OrderShoes> orderLine, AppliedCoupon coupon, Money totalPrice) {
+        Money originalTotalPrice = orderLine.stream()
+            .map(orderShoes -> orderShoes.price().multiply(orderShoes.quantity()))
+            .reduce(Money::add)
+            .orElseThrow(NoOrderItemException::new);
+
+        if(coupon == null) {
+            if(!originalTotalPrice.equals(totalPrice)) throw new InvalidTotalPriceException();
+            return;
+        }
+
+        Money disCountPrice = switch (coupon.type()){
+            case "FIXED":
+                yield  new Money(coupon.amount());
+            case "PERCENTAGE":
+                yield originalTotalPrice.multiply(coupon.amount() / 100);
+            default:
+                throw new InvalidCouponTypeException();
+        };
+
+        if(!originalTotalPrice.subtract(disCountPrice).equals(totalPrice)) throw new InvalidTotalPriceException();
+
+    }
+
+    public static UUID generateOrderCode() {
+        return UUID.randomUUID();
+    }
+
+    public void process() {
+        this.orderStatus = OrderStatus.PAYMENT_PENDING;
+    }
+
+    public Money getOriginalTotalPrice() {
+        return orderLine.stream()
+            .map(orderShoes -> orderShoes.price().multiply(orderShoes.quantity()))
+            .reduce(Money::add)
+            .orElseThrow(NoOrderItemException::new);
+    }
+
+    public void fail() {
+        this.orderStatus = OrderStatus.FAILED;
     }
 }
