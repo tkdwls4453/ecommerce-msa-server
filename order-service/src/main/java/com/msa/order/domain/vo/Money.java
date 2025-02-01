7b55879380c247a2e153130328eace1755ec641d
Yoon321 <tkdwls734@gmail.com>
2025-02-01 21:30:59 +0900

    feat: Money VO 기능 개발
    Money VO 는 모든 서비스에서 공용으로 사용될 가능성이 크기 때문에 common 모듈로 뺄 것을 고려

--- order-service/src/main/java/com/msa/order/domain/vo/Money.java
+++ order-service/src/main/java/com/msa/order/domain/vo/Money.java
@@ -1,5 +1,7 @@
 package com.msa.order.domain.vo;
 
+import java.util.Objects;
+
 public record Money(
     int amount
 ) {
@@ -10,5 +12,43 @@ public record Money(
             throw new IllegalArgumentException("amount must be greater than 0");
         }
         this.amount = amount;
+    }
+
+    public Money add(Money other){
+        return new Money(this.amount + other.amount);
+    }
+
+    public Money subtract(Money other){
+        if(this.amount < other.amount){
+            throw new IllegalArgumentException("amount must be greater than 0");
+        }
+        return new Money(this.amount - other.amount);
+    }
+
+    public Money multiply(int multiplier){
+        return new Money(this.amount * multiplier);
+    }
+
+    public Money divide(int divisor){
+        if(divisor < 0) {
+            throw new IllegalArgumentException("divisor must be greater than 0");
+        }
+        return new Money(this.amount / divisor);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Money money)) {
+            return false;
+        }
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
     }
 }
