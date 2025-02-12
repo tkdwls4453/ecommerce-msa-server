9202882225f0b10315c812b5643043101ffbb3e6
Yoon321 <tkdwls734@gmail.com>
2025-02-12 13:47:32 +0900

    feat: 결제 검증 서비스단 개발 #61

--- payment-service/src/main/java/com/msa/payment/application/service/PaymentService.java
+++ payment-service/src/main/java/com/msa/payment/application/service/PaymentService.java
@@ -3,8 +3,12 @@ package com.msa.payment.application.service;
 import com.msa.payment.application.port.in.PaymentCommandUseCase;
 import com.msa.payment.application.port.in.dto.CreatePaymentCommand;
 import com.msa.payment.application.port.in.dto.VerifyPaymentCommand;
+import com.msa.payment.application.port.out.OrderQueryPort;
 import com.msa.payment.application.port.out.PaymentCommandPort;
+import com.msa.payment.application.port.out.PaymentQueryPort;
+import com.msa.payment.application.port.out.dto.SimpleOrderResponse;
 import com.msa.payment.domain.Payment;
+import com.msa.payment.exception.PaymentNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +16,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentService implements PaymentCommandUseCase {
 
-    private final PaymentCommandPort commandPaymentPort;
+    private final PaymentCommandPort paymentCommandPort;
+    private final PaymentQueryPort paymentQueryPort;
+    private final OrderQueryPort orderQueryPort;
 
     @Override
     public Payment verify(Long customerId, VerifyPaymentCommand command) {
-        return null;
+        Payment payment = paymentQueryPort.findById(command.orderId())
+            .orElseThrow(PaymentNotFoundException::new);
+
+        SimpleOrderResponse order = orderQueryPort.findSimpleOrderByOrderId(command.orderId());
+
+        payment.verifyAndPending(customerId, order, command.paymentKey());
+        paymentCommandPort.save(payment);
+
+        return payment;
     }
 
     @Override
     public Payment tryPayment(Long customerId, CreatePaymentCommand command) {
         Payment initedPayment = Payment.init(customerId, command);
 
-        return commandPaymentPort.save(initedPayment);
+        return paymentCommandPort.save(initedPayment);
     }
 }
--- payment-service/src/main/java/com/msa/payment/exception/PaymentErrorCode.java
+++ payment-service/src/main/java/com/msa/payment/exception/PaymentErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum PaymentErrorCode implements StatusCode {
     PAYMENT_ORDER_INVALID_ERROR(HttpStatus.BAD_REQUEST, "FPY400", "결제 주문이 유효하지 않습니다."),
     PRICE_MISMATCH_ERROR(HttpStatus.BAD_REQUEST, "FPY401", "주문 금액과 결제 금액이 불일치합니다."),
-    ORDER_PERMISSION_DENIED_ERROR(HttpStatus.BAD_REQUEST, "FPY402", "주문에 대한 결제 권한이 없습니다.");
+    ORDER_PERMISSION_DENIED_ERROR(HttpStatus.BAD_REQUEST, "FPY402", "주문에 대한 결제 권한이 없습니다."),
+    PAYMENT_NOT_FOUNT(HttpStatus.BAD_REQUEST, "FPY403", "존재하지 않는 주문입니다.");
 
 
 
