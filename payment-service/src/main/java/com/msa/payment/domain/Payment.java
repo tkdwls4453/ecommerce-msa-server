8ed0396f91056ffac73600603950955eb219a63b
Yoon321 <tkdwls734@gmail.com>
2025-02-14 09:50:03 +0900

    feat: 결제 도메인이 필요한 필드 추가 #61
    결제 승인 시간과 결제 방법 필드를 추가하고 enum 으로 관리하던 payType 을 제거함
    
    payType 대신 method 필드를 사용하기로 함

--- payment-service/src/main/java/com/msa/payment/domain/Payment.java
+++ payment-service/src/main/java/com/msa/payment/domain/Payment.java
@@ -2,10 +2,13 @@ package com.msa.payment.domain;
 
 import com.msa.common.vo.Money;
 import com.msa.payment.application.port.in.dto.CreatePaymentCommand;
+import com.msa.payment.application.port.out.dto.ExternalPaymentResponse;
 import com.msa.payment.application.port.out.dto.SimpleOrderResponse;
 import com.msa.payment.exception.OrderPermissionDeniedException;
 import com.msa.payment.exception.PaymentOrderInvalidException;
 import com.msa.payment.exception.PriceMismatchException;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +16,7 @@ import lombok.Getter;
 @Getter
 public class Payment {
     private Long paymentId;
-    private PayType payType;
+    private String method;
     private PaymentStatus paymentStatus;
     private Money amount;
     private Long orderId;
@@ -23,13 +26,14 @@ public class Payment {
     private String failReason;
     private boolean cancelYN;
     private String cancelReason;
+    private LocalDateTime approvedAt;
 
     @Builder
-    private Payment(Long paymentId, PayType payType, PaymentStatus paymentStatus, Money amount, Long orderId, String orderCode,
+    private Payment(Long paymentId, String method, PaymentStatus paymentStatus, Money amount, Long orderId, String orderCode,
         Long customerId, String paymentKey, String failReason, boolean cancelYN,
         String cancelReason) {
         this.paymentId = paymentId;
-        this.payType = payType;
+        this.method = method;
         this.paymentStatus = paymentStatus;
         this.amount = amount;
         this.orderId = orderId;
@@ -60,6 +64,12 @@ public class Payment {
         this.paymentStatus = PaymentStatus.PENDING;
     }
 
+    public void confirm(String method, String approvedAt) {
+        this.method = method;
+        this.paymentStatus = PaymentStatus.COMPLETED;
+        this.approvedAt = OffsetDateTime.parse(approvedAt + "z").toLocalDateTime();
+    }
+
     private static void verifyCustomer(Long customerId, SimpleOrderResponse order) {
         if(!Objects.equals(customerId, order.customerId())) {
             throw new OrderPermissionDeniedException();
@@ -79,10 +89,13 @@ public class Payment {
         }
     }
 
-    public String getStringPayType(){
-        return this.getPayType() == null ? null : this.getPayType().toString();
+    public void fail(String failMessage) {
+        this.paymentStatus = PaymentStatus.FAILED;
+        this.failReason = failMessage;
     }
 
-
+    public String getPaymentStatusAsString(){
+        return this.paymentStatus == null ? null : paymentStatus.toString();
+    }
 }
 
