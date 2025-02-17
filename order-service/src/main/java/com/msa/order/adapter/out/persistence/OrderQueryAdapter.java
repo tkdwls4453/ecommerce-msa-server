f47e0d5a6919cc15ae17feb1ec18fabe5adea727
Yoon321 <tkdwls734@gmail.com>
2025-02-18 07:42:54 +0900

    feat: 주문 조회 기능 persistence layer 개발 #64

--- order-service/src/main/java/com/msa/order/adapter/out/persistence/OrderQueryAdapter.java
+++ order-service/src/main/java/com/msa/order/adapter/out/persistence/OrderQueryAdapter.java
@@ -3,13 +3,22 @@ package com.msa.order.adapter.out.persistence;
 import com.msa.order.application.port.out.OrderQueryPort;
 import com.msa.order.domain.Order;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
 public class OrderQueryAdapter implements OrderQueryPort {
 
+    private final OrderQueryJpaRepository orderQueryJpaRepository;
     @Override
     public Optional<Order> findById(Long orderId) {
-        return Optional.empty();
+        Optional<OrderEntity> byId = orderQueryJpaRepository.findById(orderId);
+
+        if(byId.isEmpty()) return Optional.empty();
+
+        OrderEntity orderEntity = byId.get();
+
+        return Optional.of(orderEntity.toDomain());
     }
 }
