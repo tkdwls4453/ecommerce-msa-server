b097c40aff73826457f17c83c2f3bf2ddceb6a89
Yoon321 <tkdwls734@gmail.com>
2025-02-22 20:26:33 +0900

    feat: 신발 아이디 리스트로 신발들 조회 어댑터 개발 #80

--- product-service/src/main/java/com/msa/product/adapter/out/persistence/ShoesQueryAdapter.java
+++ product-service/src/main/java/com/msa/product/adapter/out/persistence/ShoesQueryAdapter.java
@@ -3,13 +3,21 @@ package com.msa.product.adapter.out.persistence;
 import com.msa.product.application.port.out.ShoesQueryPort;
 import com.msa.product.domain.Shoes;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
 public class ShoesQueryAdapter implements ShoesQueryPort {
 
+    private final ShoesQueryJpaRepository shoesQueryJpaRepository;
+
     @Override
     public List<Shoes> findByIdIn(List<Long> idList) {
-        return List.of();
+        List<ShoesEntity> shoesEntityList = shoesQueryJpaRepository.findAllById(idList);
+
+        return shoesEntityList.stream()
+            .map(ShoesEntity::toDomain).toList();
     }
+
 }
---
+++ product-service/src/main/java/com/msa/product/adapter/out/persistence/ShoesQueryJpaRepository.java
@@ -0,0 +1,7 @@
+package com.msa.product.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoesQueryJpaRepository extends JpaRepository<ShoesEntity, Long> {
+
+}
