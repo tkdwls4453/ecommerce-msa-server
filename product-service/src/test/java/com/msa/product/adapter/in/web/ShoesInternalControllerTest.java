284ff7fb12e39b8aa289ac22baeb5e768b1efd69
Yoon321 <tkdwls734@gmail.com>
2025-02-23 12:52:13 +0900

    test: 상품 재고 복구 컨트롤러 테스트 코드 작성 #80

--- product-service/src/test/java/com/msa/product/adapter/in/web/ShoesInternalControllerTest.java
+++ product-service/src/test/java/com/msa/product/adapter/in/web/ShoesInternalControllerTest.java
@@ -10,9 +10,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.msa.product.adapter.in.web.dto.DecreaseStockRequest;
+import com.msa.product.adapter.in.web.dto.RollbackStockRequest;
 import com.msa.product.application.port.in.DecreaseStockCommand;
 import com.msa.product.application.port.in.OrderItem;
 import com.msa.product.application.port.in.ProductStockUseCase;
+import com.msa.product.application.port.in.RollbackStockCommand;
 import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -138,6 +140,112 @@ class ShoesInternalControllerTest {
                 .andExpect(jsonPath("$.code").value("F400"));
 
             verify(productStockUseCase, times(0)).decreaseStock(any(DecreaseStockCommand.class));
+        }
+    }
+
+    @Nested
+    @DisplayName("POST /internal/products/stock/rollback")
+    class Rollback {
+        @Test
+        @DisplayName("주문 상품 정보로 상품의 재고를 복구한다.")
+        void test2000() throws Exception {
+            // Given
+            OrderItem orderItem1 = OrderItem.builder()
+                .itemId(1L)
+                .quantity(2)
+                .build();
+
+            OrderItem orderItem2 = OrderItem.builder()
+                .itemId(2L)
+                .quantity(1)
+                .build();
+
+            RollbackStockRequest request = RollbackStockRequest.builder()
+                .orderLine(Arrays.asList(orderItem1, orderItem2))
+                .build();
+
+            String body = objectMapper.writeValueAsString(request);
+
+            // When Then
+
+            mockMvc.perform(post("/internal/products/stock/rollback")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(body)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("SUCCESS"))
+                .andExpect(jsonPath("$.code").value("S200"));
+
+            verify(productStockUseCase, times(1)).rollbackStock(any(RollbackStockCommand.class));
+        }
+
+        @Test
+        @DisplayName("주문 상품 아이디 없이 재고 복구 요청시 400 BAD_REQUEST 를 반환한다.")
+        void test1() throws Exception {
+            // Given
+            OrderItem orderItem1 = OrderItem.builder()
+                .itemId(null)
+                .quantity(2)
+                .build();
+
+            OrderItem orderItem2 = OrderItem.builder()
+                .itemId(2L)
+                .quantity(1)
+                .build();
+
+            RollbackStockRequest request = RollbackStockRequest.builder()
+                .orderLine(Arrays.asList(orderItem1, orderItem2))
+                .build();
+
+            String body = objectMapper.writeValueAsString(request);
+
+            // When Then
+
+            mockMvc.perform(post("/internal/products/stock/rollback")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(body)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("FAIL"))
+                .andExpect(jsonPath("$.code").value("F400"));
+
+            verify(productStockUseCase, times(0)).rollbackStock(any(RollbackStockCommand.class));
+        }
+
+        @Test
+        @DisplayName("주문 상품 수량 없이 재고 복구 요청시 400 BAD_REQUEST 를 반환한다.")
+        void test2() throws Exception {
+            // Given
+            OrderItem orderItem1 = OrderItem.builder()
+                .itemId(1L)
+                .quantity(null)
+                .build();
+
+            OrderItem orderItem2 = OrderItem.builder()
+                .itemId(2L)
+                .quantity(1)
+                .build();
+
+            RollbackStockRequest request = RollbackStockRequest.builder()
+                .orderLine(Arrays.asList(orderItem1, orderItem2))
+                .build();
+
+            String body = objectMapper.writeValueAsString(request);
+
+            // When Then
+
+            mockMvc.perform(post("/internal/products/stock/rollback")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(body)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("FAIL"))
+                .andExpect(jsonPath("$.code").value("F400"));
+
+            verify(productStockUseCase, times(0)).rollbackStock(any(RollbackStockCommand.class));
         }
     }
 }
